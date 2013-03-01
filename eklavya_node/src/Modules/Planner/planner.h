67fd147841e5cb8f49f75a893aeeb207eedac2bf
Samuel Anudeep <banmido69@gmail.com>
2013-03-01 21:00:08 +0530

    Added GPS and Planner Files

---
+++ eklavya_node/src/Modules/Planner/planner.h
@@ -0,0 +1,23 @@
+#ifndef _PLANNER_H_
+#define _PLANNER_H_
+
+#include "ros/ros.h"
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <boost/bind.hpp>
+#include "../../eklavya2.h"
+
+namespace planner_space {
+  typedef struct command {
+    int left_velocity, right_velocity;
+  } command;
+  
+  class Planner {
+  public:
+    static void loadPlanner();
+    static vector<Triplet> findPath(Triplet bot, Triplet target);
+    static void closePlanner();
+  };
+}
+
+#endif
